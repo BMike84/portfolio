7d980a6ef1d42fc0a1f83cffaaddcc5c34bf9504
Michael Buffone <m_buffone@hotmail.com>
2022-09-15 16:12:08 -0400

    made navbar responsive made a few minor style adjustments

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
-import React from "react";
+import { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { Link } from "react-scroll";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 //Scss Font Awesome
 import "./Navbar.scss";
@@ -9,51 +10,76 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Navbar = () => {
   useNavigate();
 
-  return (
-    <nav id="nav">
-      <div className="logo">
-        <NavLink to="/" duration={1000}>
-          <span>
-            <FontAwesomeIcon icon="fa-solid fa-code" />
-          </span>
-          Michael Buffone
-        </NavLink>
-      </div>
-      <div className="links">
-        <NavLink to="/">Home</NavLink>
-        {window.location.href === "http://localhost:3000/" ? (
-          <Link
-            to="services"
-            spy={true}
-            smooth={true}
-            duration={1000}
-            offset={-100}
-          >
-            Services
-          </Link>
-        ) : (
-          <NavLink to="/">Services</NavLink>
-        )}
-        {window.location.href === "http://localhost:3000/" ? (
-          <Link
-            to="projects"
-            spy={true}
-            smooth={true}
-            duration={1000}
-            offset={30}
-          >
-            Projects
-          </Link>
-        ) : (
-          <NavLink to="/">Projects</NavLink>
-        )}
-        <NavLink to="about">About Me</NavLink>
-      </div>
+  // adds nav button on smller screen
+  const [showLinks, setShowLinks] = useState(true);
 
-      <NavLink className="btn-contact" to="contact">
-        Contact Me
-      </NavLink>
-    </nav>
+  // makes navbar color on scroll
+  const [navColor, setNavColor] = useState(false);
+
+  const changeColor = () => {
+    if (window.scrollY >= 65) {
+      setNavColor(true);
+    } else {
+      setNavColor(false);
+    }
+  };
+
+  window.addEventListener("scroll", changeColor);
+
+  return (
+    <>
+      <nav id="nav" className={navColor ? "nav-bg" : ""}>
+        <div className="logo">
+          <NavLink to="/" duration={1000}>
+            <span>
+              <FontAwesomeIcon icon="fa-solid fa-code" />
+            </span>
+            Michael Buffone
+          </NavLink>
+        </div>
+        <div className="links links-media" id={showLinks ? "hidden" : ""}>
+          <NavLink to="/">Home</NavLink>
+          {window.location.href === "http://localhost:3000/" ? (
+            <Link
+              to="services"
+              spy={true}
+              smooth={true}
+              duration={1000}
+              offset={-100}
+            >
+              Services
+            </Link>
+          ) : (
+            <NavLink to="/">Services</NavLink>
+          )}
+          {window.location.href === "http://localhost:3000/" ? (
+            <Link
+              to="projects"
+              spy={true}
+              smooth={true}
+              duration={1000}
+              offset={30}
+            >
+              Projects
+            </Link>
+          ) : (
+            <NavLink to="/">Projects</NavLink>
+          )}
+          <NavLink to="about">About Me</NavLink>
+          <NavLink id="contact-media" className="bars" to="contact">
+            Contact Me
+          </NavLink>
+        </div>
+        <div className="contact-link links-media">
+          <NavLink className="btn-contact" to="contact">
+            Contact Me
+          </NavLink>
+        </div>
+        <button className="bars" onClick={() => setShowLinks(!showLinks)}>
+          <FaBars />
+        </button>
+      </nav>
+    </>
   );
 };
 
